e95ae4d0d9c73dd80c1a4a65112f45791adaa38d
Kevin Donovan <donovankevin58@gmail.com>
2025-01-21 23:09:33 -0500

    added Repos for GamePlatform, Platform

--- server/reviewtique/src/main/java/learn/review_tique/data/GamePlatformJdbcTemplateRepository.java
+++ server/reviewtique/src/main/java/learn/review_tique/data/GamePlatformJdbcTemplateRepository.java
@@ -1,5 +1,6 @@
 package learn.review_tique.data;
 
+import learn.review_tique.data.mappers.GamePlatformMapper;
 import learn.review_tique.models.GamePlatform;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -15,16 +16,27 @@ public class GamePlatformJdbcTemplateRepository implements GamePlatformRepositor
 
     @Override
     public boolean add(GamePlatform gamePlatform) {
-        return false;
+        final String sql = "insert into game_platform (game_id, platform_id)"
+                + " values (?, ?);";
+
+        return jdbcTemplate.update(sql, gamePlatform.getGameId(), gamePlatform.getPlatform().getPlatformId()) > 0;
     }
 
     @Override
     public List<GamePlatform> findByGameId(int gameId) {
-        return List.of();
+        final String sql = "select gp.game_id, gp.platform_id, p.platform_name"
+                + " from game_platform gp"
+                + " join platform p on gp.platform_id = p.platform_id"
+                + " where gp.game_id = ?;";
+
+        return jdbcTemplate.query(sql, new GamePlatformMapper(), gameId);
     }
 
     @Override
     public boolean deleteById(int gameId, int platformId) {
-        return false;
+        final String sql = "delete from game_platform "
+                + "where game_id = ? and platform_id = ?;";
+
+        return jdbcTemplate.update(sql, gameId, platformId) > 0;
     }
 }
--- server/reviewtique/src/main/java/learn/review_tique/data/PlatformJdbcTemplateRepository.java
+++ server/reviewtique/src/main/java/learn/review_tique/data/PlatformJdbcTemplateRepository.java
@@ -1,8 +1,13 @@
 package learn.review_tique.data;
 
+import learn.review_tique.data.mappers.PlatformMapper;
 import learn.review_tique.models.Platform;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 public class PlatformJdbcTemplateRepository implements PlatformRepository {
@@ -15,26 +20,50 @@ public class PlatformJdbcTemplateRepository implements PlatformRepository {
 
     @Override
     public List<Platform> findAll() {
-        return List.of();
+        final String sql = "select platform_id, platform_name from platform;";
+
+        return jdbcTemplate.query(sql, new PlatformMapper());
     }
 
     @Override
     public Platform findById(int platformId) {
-        return null;
+        final String sql = "select platform_id, platform_name from platform"
+                + " where platform_id = ?;";
+
+        return jdbcTemplate.query(sql, new PlatformMapper(), platformId).stream().findFirst().orElse(null);
     }
 
     @Override
     public Platform add(Platform platform) {
-        return null;
+        final String sql = "insert into platform (platform_name) "
+                + "values (?);";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, platform.getPlatformName());
+            return ps;
+        }, keyHolder);
+
+        if(rowsAffected <= 0)
+            return null;
+
+        platform.setPlatformId(keyHolder.getKey().intValue());
+        return platform;
     }
 
     @Override
     public boolean update(Platform platform) {
-        return false;
+        final String sql = "update platform set"
+                + " platform_name = ?"
+                + " where platform_id = ?;";
+
+        return jdbcTemplate.update(sql, platform.getPlatformName(), platform.getPlatformId()) > 0;
     }
 
     @Override
     public boolean deleteById(int platformId) {
-        return false;
+        final String sql = "delete from platform where platform_id = ?;";
+
+        return jdbcTemplate.update(sql, platformId) > 0;
     }
 }

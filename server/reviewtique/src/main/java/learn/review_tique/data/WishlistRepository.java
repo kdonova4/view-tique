a0faa30f5db479094261d5bacc9f94c8898ab519
Kevin Donovan <donovankevin58@gmail.com>
2025-01-22 20:50:42 -0500

    made combo of revew_id and app_user_id unique in review_reaction table, completed wishlist and ReviewReaction repo

--- server/reviewtique/src/main/java/learn/review_tique/data/WishlistRepository.java
+++ server/reviewtique/src/main/java/learn/review_tique/data/WishlistRepository.java
@@ -1,4 +1,14 @@
 package learn.review_tique.data;
 
+import learn.review_tique.models.Wishlist;
+
+import java.util.List;
+
 public interface WishlistRepository {
+
+    List<Wishlist> findByUserId(int userId);
+
+    Wishlist add(Wishlist wishlist);
+
+    boolean deleteById(int wishlistId);
 }

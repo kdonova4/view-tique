b3bc39bf372241e94f637a0e098830728110acb5
Kevin Donovan <donovankevin58@gmail.com>
2025-03-25 18:21:18 -0400

    Added preLoad Genre and Company methods, tested both, and implemented paginated fetching for companies

--- server/reviewtique/src/test/java/learn/review_tique/domain/IGDBDataImporterTest.java
+++ server/reviewtique/src/test/java/learn/review_tique/domain/IGDBDataImporterTest.java
@@ -18,10 +18,24 @@ public class IGDBDataImporterTest {
 
 
     @Test
-    void shouldPopulate500Platforms() {
-
+    void shouldPopulate219Platforms() {
         Map<Integer, String> platforms = importer.preloadPlatforms("https://api.igdb.com/v4/platforms");
         assertEquals(219, platforms.size());
         assertEquals("Xbox 360", platforms.get(12));
+    }
+
+    @Test
+    void shouldPopulate23Genres() {
+        Map<Integer, String> genres = importer.preloadGenres("https://api.igdb.com/v4/genres");
+        assertEquals(23, genres.size());
+    }
+
+
+    @Test
+    void shouldPopulate58712Companies() {
+        Map<Integer, String> companies = importer.preloadCompanies("https://api.igdb.com/v4/companies");
+        assertEquals(58712, companies.size());
+
+        assertEquals("Nitrome", companies.get(15443));
     }
 }

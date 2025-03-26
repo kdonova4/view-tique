ef61ec3e6c2a7042cf5a07202d6d93cc65d9a0c4
Kevin Donovan <donovankevin58@gmail.com>
2025-03-26 19:43:20 -0400

    Added Cover and InvolvedCompanies Preload mehtods, and tests

--- server/reviewtique/src/test/java/learn/review_tique/domain/IGDBDataImporterTest.java
+++ server/reviewtique/src/test/java/learn/review_tique/domain/IGDBDataImporterTest.java
@@ -1,11 +1,13 @@
 package learn.review_tique.domain;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,14 +30,29 @@ public class IGDBDataImporterTest {
     void shouldPopulate23Genres() {
         Map<Integer, String> genres = importer.preloadGenres("https://api.igdb.com/v4/genres");
         assertEquals(23, genres.size());
+        assertEquals("Shooter", genres.get(5));
     }
 
 
     @Test
     void shouldPopulate58712Companies() {
         Map<Integer, String> companies = importer.preloadCompanies("https://api.igdb.com/v4/companies");
-        assertEquals(58712, companies.size());
 
         assertEquals("Nitrome", companies.get(15443));
+    }
+
+    @Test
+    void shouldPopulate246660Covers() {
+        Map<Integer, String> covers = importer.preLoadCovers("https://api.igdb.com/v4/covers");
+
+        assertEquals("//images.igdb.com/igdb/image/upload/t_cover_big/co9kt3.jpg", covers.get(446871));
+    }
+
+    @Test
+    void shouldPopulate220837InvolvedCompanies() {
+        Map<Integer, Pair<Integer, Boolean>> involvedCompanies = importer.preloadInvolvedCompanies("https://api.igdb.com/v4/involved_companies");
+
+        assertEquals(true, involvedCompanies.get(95000).getRight());
+        assertEquals(14486, involvedCompanies.get(95000).getLeft());
     }
 }
